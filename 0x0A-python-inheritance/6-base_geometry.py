51e8a07f48f91cfe8982063765cabab0f4a492fe
EduNgaira <ngairaedwin@gmail.com>
2023-04-13 12:49:16 -0700

    Add

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function 5-base_geometry.py container
+"""
+
+
+class BaseGeometry:
+    """Public attribute area class"""
+    def area(self):
+        """raises exception when function is called"""
+        raise Exception("area() is not implemented")
