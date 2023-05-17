74bf13671236f16f71b812c12f4c627b0fb8471f
EduNgaira <ngairaedwin@gmail.com>
2023-05-17 08:11:07 -0700

    Files

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,8 @@
--- Lists all shows in the database.
--- Records ordered by ascending tv_shows.title and tv_show_genres.genre_id.
+-- Lists all shows
+-- Displays NULL for shows without genres.
+-- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
        LEFT JOIN `tv_show_genres` AS g
        ON s.`id` = g.`show_id`
-       WHERE g.`genre_id` IS NULL
  ORDER BY s.`title`, g.`genre_id`;
