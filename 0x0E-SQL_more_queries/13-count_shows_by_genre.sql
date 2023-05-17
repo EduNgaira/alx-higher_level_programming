5443d9bb19b32819c14ac9de24d1687853fccaa6
EduNgaira <ngairaedwin@gmail.com>
2023-05-17 08:13:53 -0700

    Files

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,11 +1,10 @@
--- Lists all Dexter genres.
--- Ordered by ascending genre name.
-SELECT g.`name`
+-- Lists all genres with the number of shows linked to each.
+-- Don't display without linked shows.
+-- Order records by descending number of shows linked.
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
   FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON g.`id` = s.`genre_id`
-
-       INNER JOIN `tv_shows` AS t
-       ON t.`id` = s.`show_id`
-       WHERE t.`title` = "Dexter"
- ORDER BY g.`name`;
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
