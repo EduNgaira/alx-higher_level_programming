68cc66e5806dff9f436a78e6e52ac09b80f8cdf0
EduNgaira <ngairaedwin@gmail.com>
2023-05-17 07:41:07 -0700

    Cities

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- Lists CA cities
+-- Results ordered by ascending cities.id.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
